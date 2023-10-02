283146e27796cabc25161fc37a787a59731c3d4d
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-10-01 18:10:37 -0600

    Finished course

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import './style.css'
+import './style.css';
+// import './clean-code/04-homework';
+// import './clean-code/05-dry';
+// import './solid/01-srp';
+// import './solid/02-ocp-a';
+// import './solid/03-liskov-a';
+import './solid/05-dependency-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
